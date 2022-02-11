eddeb840871b78e5605f94f04a23d468e8cf822f
chaitanyasaibeesabatuni <87239340+chaitanyasaibeesabatuni@users.noreply.github.com>
2022-02-11 15:49:07 +0530

    Update project-mail.py

--- project-mail.py
+++ project-mail.py
@@ -1,24 +1,22 @@
 import smtplib
-a=False
-sender_email = input("Enter your email address :")
-sender_password = input("enter your password :")
-receiver_email = input("enter the email to send :")
-
-subject = input("Enter the subject :")
-body = input("Enter the message of your mail")
-message = """\
-Subject:
-{}
-{}""".format(subject, body)
-
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
 try:
-    smtp.login(sender_email, sender_password)
-    smtp.sendmail(sender_email, receiver_email, message)
-    print("Email Sent Successfully")
+    sender_email = input("Enter your email address :")
+    sender_password = input("enter your password :")
+    receiver_email = input("enter the email to send :")
+
+    subject = input("Enter the subject :")
+    body = input("Enter the message of your mail")
+    message = """\
+    Subject:
+    {}
+    {}""".format(subject, body)
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(sender_email, sender_password)
+        smtp.sendmail(sender_email, receiver_email, message)
+        print("Email Sent Successfully")
 except:
-    print("Enter Valid Credentials")
-    print("Invalid Username or Password")
+    print("Invalid UserId or Password")
